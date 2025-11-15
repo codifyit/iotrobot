8f47ef19ba3232b4bf2952ce505ba5a3a7a4e235
my name <rmenmar264@g.educaand.es>
2025-11-15 09:59:22 +0100

    AdeeptArmRobot new display function

--- theprojects/roboticarm/Code/9. Processing/AdeeptArmRobot/AdeeptArmRobot.ino
+++ theprojects/roboticarm/Code/9. Processing/AdeeptArmRobot/AdeeptArmRobot.ino
@@ -6,8 +6,15 @@ Website: www.adeept.com
 E-mail: support@adeept.com
 Author: Tom
 Date: 2020/12/12
+Released: 2025/11/14
+Author: Rafa
 ***********************************************************/
 #include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define OLED_RESET     4
+Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
 int servopin1 = 9;    //Define servo interface digital interface 9
 int servopin2 = 6;    //Define servo interface digital interface 6
 int servopin3 = 5;    //Define servo interface digital interface 5
@@ -48,9 +55,24 @@ void setup() {
   servo2.write(90);
   servo3.write(90);
   servo4.write(90);
-  servo5.write(90); 
+  servo5.write(105); 
   delay(20);
   //servo1Pulse(90);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.setTextColor(WHITE);//Sets the font display color
+  display.clearDisplay();//cls
+  display.clearDisplay();
+  //Set the font size
+  display.setTextSize(2);
+  //Set the display location
+  display.setCursor(3,1);
+  //String displayed
+  display.print("IES Axati");
+  display.setCursor(3,20);
+  //String displayed
+  display.print("Waiting...");
+  //Began to show
+  display.display();
 }
 void loop() {
   delay(20);
@@ -98,9 +120,6 @@ void loop() {
       if(servo3Angle<=0){servo3Angle=0;}
       servo3.write(servo3Angle);
     }
-
-
-
     
     if (moveServoData == 101 || moveServoData == 69) {//e
       autoLock = false;
@@ -120,7 +139,7 @@ void loop() {
       autoLock = false;
       closeLock=false;
       servo5Angle+=10;
-      if(servo5Angle>=90){servo5Angle=90;}
+      if(servo5Angle>=105){servo5Angle=105;}
       servo5.write(servo5Angle);
     }
     if(moveServoData == 119 || moveServoData == 87) {//w
@@ -137,5 +156,72 @@ void loop() {
     }
   }
   while(Serial.available()>0);
+  if(moveServoData>0)
+    displayInfo(moveServoData);
+}
 
+void displayInfo(int moveServoData){
+  String text1,text2;
+
+  display.clearDisplay();
+  //Set the font size
+  display.setTextSize(2);
+  //Set the display location
+  display.setCursor(3,1);
+  //String displayed
+  display.print("IES Axati");
+  display.display();
+
+  if (moveServoData == 111 || moveServoData == 79) {
+    text1="Base";
+    text2="left!";
+  }else if(moveServoData == 112 || moveServoData == 80){
+    text1="Base";
+    text2="right!";
+  }else if (moveServoData == 117 || moveServoData == 85) {
+    text1="Shoulder";
+    text2="opening!";
+  }else if (moveServoData == 105 || moveServoData == 73) {
+    text1="Shoulder";
+    text2="closing!";
+  }else if (moveServoData == 116 || moveServoData == 84) {
+    text1="Elbow";
+    text2="closing!";
+  }else if (moveServoData == 121 || moveServoData == 89) {
+    text1="Elbow";
+    text2="opening!";
+  }else if (moveServoData == 101 || moveServoData == 69) {
+    text1="Rotate";
+    text2="left!";
+  }else if (moveServoData == 114 || moveServoData == 82) {
+    text1="Rotate";
+    text2="right!";
+  }else if(moveServoData == 113 || moveServoData == 81) {
+    text1="Gripper";
+    text2="closing!";
+  }else if(moveServoData == 119 || moveServoData == 87) {
+    text1="Gripper";
+    text2="opening!";
+  }else if(moveServoData == 110 || moveServoData ==78){
+  }
+
+  display.setCursor(3,20);
+  display.print(text1);
+  display.setCursor(3,40);
+  display.print(text2);
+
+  display.display();
+  //delay(1000);
+  display.clearDisplay();
+  //Set the font size
+  display.setTextSize(2);
+  //Set the display location
+  display.setCursor(3,1);
+  //String displayed
+  display.print("IES Axati");
+  display.setCursor(3,20);
+  //String displayed
+  display.print("Waiting...");
+  //Began to show
+  display.display();
 }
